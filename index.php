085ea140261c8503e8b0bbd360630974753d05dc
Tian <tianryno01@gmail.com>
2025-01-02 17:45:10 +0200

    Update index.php
    Update website description test

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Your new description goes here. This is a short and concise summary of your website's purpose and content.">
     <title>Elavelle</title>
     <link rel="stylesheet" href="css/main.css">
 </head>
